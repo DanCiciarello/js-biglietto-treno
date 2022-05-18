5a221003bf61eb863c1114c89d9655362caeedb5
DanCiciarello <me@danieleciciarello.com>
2022-05-18 14:03:57 +0200

    Completed exercise

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// User declares year of birth
+const userYearOfBirth = prompt("Insert your year of birth");
+
+// User declares distance
+const userKilometres = prompt("Insert the kilometres that you need to do");
+
+// Create a variable with current year
+const currentDate = new Date();
+
+// Calculate the age of the user
+const userAgeCalc = currentDate.getFullYear() - userYearOfBirth;
+
+// Calculate the prices
+const ticketPriceFull = 0.21 * userKilometres;
+const ticketPriceYoung = ticketPriceFull - (ticketPriceFull * 0.2);
+const ticketPriceSenior = ticketPriceFull - (ticketPriceFull * 0.4);
+
+// Get the HTML elements
+const userAge = document.getElementById("user-age");
+const userDistance = document.getElementById("user-distance");
+const userPrice = document.getElementById("final-price");
+
+// Assign value to HTML elements
+userAge.innerHTML = `Età: ${userAgeCalc} anni`;
+userDistance.innerHTML = `Distanza: ${userKilometres} km`;
+
+if (userAgeCalc < 18) {
+    userPrice.innerHTML = `Prezzo: ${ticketPriceYoung} €`;
+} else if (userAgeCalc >= 65) {
+    userPrice.innerHTML = `Prezzo: ${ticketPriceSenior} €`;
+} else {
+    userPrice.innerHTML = `Prezzo: ${ticketPriceFull} €`;
+}
